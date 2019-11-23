faab2f77ce5d880ec0fcebd24b581cef7e8e9763
Alejandro Ulate <alejandro.ulate@buildto.io>
2019-11-22 18:11:46 -0600

    Changelog:Migrated base project to swift/kotlin - Added effective dart style.

--- ios/Classes/HttpInterceptorPlugin.m
+++ ios/Classes/HttpInterceptorPlugin.m
@@ -1,20 +1,8 @@
 #import "HttpInterceptorPlugin.h"
+#import <http_interceptor/http_interceptor-Swift.h>
 
 @implementation HttpInterceptorPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"http_interceptor"
-            binaryMessenger:[registrar messenger]];
-  HttpInterceptorPlugin* instance = [[HttpInterceptorPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+  [SwiftHttpInterceptorPlugin registerWithRegistrar:registrar];
 }
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
 @end
