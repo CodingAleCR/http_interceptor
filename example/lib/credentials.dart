d4f9812861661e79a0ef5d2d48eaf7163a60734f
Alejandro Ulate <alejandro.ulate@buildto.io>
2020-04-24 13:40:04 -0600

    Removing credentials in example.

--- example/lib/credentials.dart
+++ example/lib/credentials.dart
@@ -1,2 +1,2 @@
 // Replace this if you are going to run the example. You can get your own at https://openweathermap.org/
-const String OPEN_WEATHER_API_KEY = "b04a4a7c5a71de47d7cdb0c7607c5d14";
+const String OPEN_WEATHER_API_KEY = "YOUR-KEY-HERE";
