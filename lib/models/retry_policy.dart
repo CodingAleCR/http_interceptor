2de8f2fb609d4bc9d34498bba64495310472ef0c
Wes Ehrlichman <wes.ehrlichman@asynchrony.com>
2020-05-19 09:52:00 -0500

    Simple tests around retry_policy - Wes

--- lib/models/retry_policy.dart
+++ lib/models/retry_policy.dart
@@ -2,7 +2,6 @@ import 'package:http/http.dart';
 
 abstract class RetryPolicy {
   bool shouldAttemptRetryOnException(Exception reason) => false;
-  Future<bool> shouldAttemptRetryOnResponse(Response response) async =>
-      Future.value(false);
+  Future<bool> shouldAttemptRetryOnResponse(Response response) async => false;
   final int maxRetryAttempts = 1;
 }
