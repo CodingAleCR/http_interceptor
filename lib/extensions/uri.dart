8e0ef65db8a660b8d5e8bd34936bd24e1231013e
Klemen Tušar <techouse@gmail.com>
2025-05-06 22:00:33 +0100

    :art: facelift code

--- lib/extensions/uri.dart
+++ lib/extensions/uri.dart
@@ -1,16 +1,17 @@
 import 'package:http_interceptor/extensions/string.dart';
 import 'package:http_interceptor/utils/utils.dart';
 
-/// Extends `Uri` to allow adding parameters to already created intstances
+/// Extends `Uri` to allow adding parameters to already created instances.
 extension AddParameters on Uri {
   /// Returns a new [Uri] instance based on `this` and adds [parameters].
-  Uri addParameters(Map<String, dynamic>? parameters) => parameters != null
-      ? buildUrlString(
-          origin + path,
-          {
-            ...queryParametersAll,
-            ...parameters,
-          },
-        ).toUri()
-      : this;
+  Uri addParameters([Map<String, dynamic>? parameters]) =>
+      parameters?.isNotEmpty ?? false
+          ? buildUrlString(
+              "$origin$path",
+              {
+                ...queryParametersAll,
+                ...?parameters,
+              },
+            ).toUri()
+          : this;
 }
